993e8845c4005fd578e42d0ea78cf2554a3ce134
Vloits <vloits19@gmail.com>
2026-03-12 13:50:27 +0900

    Update BetaCapsule.tsx

--- src/components/BetaCapsule.tsx
+++ src/components/BetaCapsule.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Zap } from 'lucide-react';
-import henshinSound from '@/assest/sound/Henshin.mp3';
+import henshinSound from '@/assets/sound/Henshin.mp3';
 interface BetaCapsuleProps {
   onTransform: () => void;
 }
